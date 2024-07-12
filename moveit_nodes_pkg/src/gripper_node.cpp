1d05dfc20c2594ea1119429405582251603c0a6f
newtonjeri <newtonkaris45@gmail.com>
2024-07-12 16:58:41 +0300

    node to test gripper opening and closing

--- moveit_nodes_pkg/src/gripper_node.cpp
+++ moveit_nodes_pkg/src/gripper_node.cpp
@@ -1,23 +1,29 @@
 #include <rclcpp/rclcpp.hpp>
 #include <moveit/move_group_interface/move_group_interface.h>
 
+    //Logger
+    auto const LOGGER = rclcpp::get_logger("Gripper Logeer");
 
 int main(int argc, char **argv){
     rclcpp::init(argc, argv);
     rclcpp::NodeOptions node_options;
     node_options.automatically_declare_parameters_from_overrides(true);
 
-    //Logger
-    auto const LOGGER = rclcpp::get_logger("Gripper Logeer");
+
 
     auto const node = std::make_shared<rclcpp::Node>("gripper_node", node_options);
     std::string group_name = "xarm_gripper";
     // Move group interface 
     auto gripper_group = moveit::planning_interface::MoveGroupInterface(node, group_name);
 
+    RCLCPP_INFO(LOGGER, "Planning frame: %s", gripper_group.getPlanningFrame().c_str());
+    RCLCPP_INFO(LOGGER, "End effector link: %s", gripper_group.getEndEffectorLink().c_str());
+
+    double gripper_open_joint_value = 0;
+    double gripper_close_joint_value = 0.8;
 
     // Open gripper
-    gripper_group.setJointValueTarget({0.84});
+    gripper_group.setJointValueTarget({gripper_open_joint_value});
 
     auto const [success, plan] = [&gripper_group]{
         moveit::planning_interface::MoveGroupInterface::Plan msg;
@@ -26,11 +32,11 @@ int main(int argc, char **argv){
     }();
 
     if(success){
-        gripper_group.execute(plan);
+        gripper_group.move();
     }
 
     // Close gripper
-    gripper_group.setJointValueTarget({0.84});
+    gripper_group.setJointValueTarget({gripper_close_joint_value});
 
     auto const [success_, plan_] = [&gripper_group]{
         moveit::planning_interface::MoveGroupInterface::Plan msg;
@@ -39,7 +45,7 @@ int main(int argc, char **argv){
     }();
 
     if(success_){
-        gripper_group.execute(plan_);
+        gripper_group.move();
     }
 
     rclcpp::shutdown();

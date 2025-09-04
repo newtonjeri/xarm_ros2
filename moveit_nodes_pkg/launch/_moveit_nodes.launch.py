5a474f85bb087c29eec91fd46f2fe04c6fa0f0e6
newtonjeri <newtonkaris45@gmail.com>
2025-09-04 17:09:26 +0300

    feat: add launch file for MoveIt configuration and node setup

---
+++ moveit_nodes_pkg/launch/_moveit_nodes.launch.py
@@ -0,0 +1,133 @@
+#!/usr/bin/env python3
+
+import yaml
+import json
+from launch import LaunchDescription
+from launch.actions import OpaqueFunction
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+from uf_ros_lib.moveit_configs_builder import MoveItConfigsBuilder
+
+
+def launch_setup(context, *args, **kwargs):
+    # Robot configuration parameters
+    dof = LaunchConfiguration('dof', default=7)
+    robot_type = LaunchConfiguration('robot_type', default='xarm')
+    prefix = LaunchConfiguration('prefix', default='')
+    hw_ns = LaunchConfiguration('hw_ns', default='xarm')
+    limited = LaunchConfiguration('limited', default=True)
+    effort_control = LaunchConfiguration('effort_control', default=False)
+    velocity_control = LaunchConfiguration('velocity_control', default=False)
+    model1300 = LaunchConfiguration('model1300', default=False)
+    robot_sn = LaunchConfiguration('robot_sn', default='')
+    attach_to = LaunchConfiguration('attach_to', default='world')
+    attach_xyz = LaunchConfiguration('attach_xyz', default='"0 0 0"')
+    attach_rpy = LaunchConfiguration('attach_rpy', default='"0 0 0"')
+    mesh_suffix = LaunchConfiguration('mesh_suffix', default='stl')
+    kinematics_suffix = LaunchConfiguration('kinematics_suffix', default='')
+
+    # Gripper and sensor configurations
+    add_gripper = LaunchConfiguration('add_gripper', default=False)
+    add_vacuum_gripper = LaunchConfiguration('add_vacuum_gripper', default=False)
+    add_bio_gripper = LaunchConfiguration('add_bio_gripper', default=False)
+    add_realsense_d435i = LaunchConfiguration('add_realsense_d435i', default=False)
+    add_d435i_links = LaunchConfiguration('add_d435i_links', default=True)
+    add_other_geometry = LaunchConfiguration('add_other_geometry', default=False)
+    geometry_type = LaunchConfiguration('geometry_type', default='box')
+    geometry_mass = LaunchConfiguration('geometry_mass', default=0.1)
+    geometry_height = LaunchConfiguration('geometry_height', default=0.1)
+    geometry_radius = LaunchConfiguration('geometry_radius', default=0.1)
+    geometry_length = LaunchConfiguration('geometry_length', default=0.1)
+    geometry_width = LaunchConfiguration('geometry_width', default=0.1)
+    geometry_mesh_filename = LaunchConfiguration('geometry_mesh_filename', default='')
+    geometry_mesh_origin_xyz = LaunchConfiguration('geometry_mesh_origin_xyz', default='"0 0 0"')
+    geometry_mesh_origin_rpy = LaunchConfiguration('geometry_mesh_origin_rpy', default='"0 0 0"')
+    geometry_mesh_tcp_xyz = LaunchConfiguration('geometry_mesh_tcp_xyz', default='"0 0 0"')
+    geometry_mesh_tcp_rpy = LaunchConfiguration('geometry_mesh_tcp_rpy', default='"0 0 0"')
+
+    # Build MoveIt configuration
+    moveit_config_dump = LaunchConfiguration('moveit_config_dump', default='')
+    moveit_config_dump = moveit_config_dump.perform(context)
+    moveit_config_dict = yaml.load(moveit_config_dump, Loader=yaml.FullLoader) if moveit_config_dump else {}
+
+    if not moveit_config_dict:
+        moveit_config = MoveItConfigsBuilder(
+            context=context,
+            dof=dof,
+            robot_type=robot_type,
+            prefix=prefix,
+            hw_ns=hw_ns,
+            limited=limited,
+            effort_control=effort_control,
+            velocity_control=velocity_control,
+            model1300=model1300,
+            robot_sn=robot_sn,
+            attach_to=attach_to,
+            attach_xyz=attach_xyz,
+            attach_rpy=attach_rpy,
+            mesh_suffix=mesh_suffix,
+            kinematics_suffix=kinematics_suffix,
+            add_gripper=add_gripper,
+            add_vacuum_gripper=add_vacuum_gripper,
+            add_bio_gripper=add_bio_gripper,
+            add_realsense_d435i=add_realsense_d435i,
+            add_d435i_links=add_d435i_links,
+            add_other_geometry=add_other_geometry,
+            geometry_type=geometry_type,
+            geometry_mass=geometry_mass,
+            geometry_height=geometry_height,
+            geometry_radius=geometry_radius,
+            geometry_length=geometry_length,
+            geometry_width=geometry_width,
+            geometry_mesh_filename=geometry_mesh_filename,
+            geometry_mesh_origin_xyz=geometry_mesh_origin_xyz,
+            geometry_mesh_origin_rpy=geometry_mesh_origin_rpy,
+            geometry_mesh_tcp_xyz=geometry_mesh_tcp_xyz,
+            geometry_mesh_tcp_rpy=geometry_mesh_tcp_rpy,
+        ).to_moveit_configs()
+        moveit_config_dict = moveit_config.to_dict()
+    
+    # Prepare MoveGroup interface parameters
+    move_group_interface_params = {
+        'robot_description': moveit_config_dict['robot_description'],
+        'robot_description_semantic': moveit_config_dict['robot_description_semantic'],
+        'robot_description_kinematics': moveit_config_dict['robot_description_kinematics'],
+    }
+
+    # Node configuration parameters
+    node_executable = LaunchConfiguration('node_executable')
+    node_name = LaunchConfiguration('node_name', default=node_executable)
+    node_parameters = LaunchConfiguration('node_parameters', default={})
+    group_name = LaunchConfiguration('group_name', default='xarm7')
+
+    # Parse additional node parameters
+    try:
+        additional_node_parameters = json.loads(node_parameters.perform(context))
+    except:
+        additional_node_parameters = {}
+
+    # Create the node with MoveIt parameters
+    moveit_node = Node(
+        name=node_name,
+        package='moveit_nodes_pkg',
+        executable=node_executable,
+        output='screen',
+        parameters=[
+            move_group_interface_params,
+            {
+                'group_name': group_name,
+                'robot_type': robot_type,
+                'dof': dof,
+                'prefix': prefix
+            },
+            additional_node_parameters,
+        ],
+    )
+
+    return [moveit_node]
+
+
+def generate_launch_description():
+    return LaunchDescription([
+        OpaqueFunction(function=launch_setup)
+    ])

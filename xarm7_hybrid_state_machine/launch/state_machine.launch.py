d948364d345d0f0a40e44b64fbf540972057c404
newtonjeri <newtonkaris45@gmail.com>
2025-09-08 17:16:56 +0300

    feat: add launch file for hybrid state machine with multithreading support

---
+++ xarm7_hybrid_state_machine/launch/state_machine.launch.py
@@ -0,0 +1,139 @@
+#!/usr/bin/env python3
+# Copyright 2025 Virtual Mechatronics Labs DeKUT All Rights Reserved.
+#
+# Launch file for the hybrid state machine with proper multithreading
+
+from launch import LaunchDescription
+from launch_ros.actions import ComposableNodeContainer
+from launch_ros.descriptions import ComposableNode
+
+
+from launch import LaunchDescription
+from launch_ros.descriptions import ComposableNode
+from launch.actions import IncludeLaunchDescription, OpaqueFunction
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
+from launch_ros.substitutions import FindPackageShare
+import yaml
+from uf_ros_lib.moveit_configs_builder import MoveItConfigsBuilder
+
+from launch_ros.actions import (
+    ComposableNodeContainer,
+)
+
+def launch_setup(context, *args, **kwargs):
+    # Robot configuration parameters with defaults
+    dof = LaunchConfiguration('dof', default='7')
+    robot_type = LaunchConfiguration('robot_type', default='xarm')
+    prefix = LaunchConfiguration('prefix', default='')
+    hw_ns = LaunchConfiguration('hw_ns', default='xarm')
+    limited = LaunchConfiguration('limited', default=True)
+    effort_control = LaunchConfiguration('effort_control', default=False)
+    velocity_control = LaunchConfiguration('velocity_control', default=False)
+    model1300 = LaunchConfiguration('model1300', default=False)
+    robot_sn = LaunchConfiguration('robot_sn', default='')
+    attach_to = LaunchConfiguration('attach_to', default='world')
+    attach_xyz = LaunchConfiguration('attach_xyz', default='"0 0 0"')
+    attach_rpy = LaunchConfiguration('attach_rpy', default='"0 0 0"')
+    mesh_suffix = LaunchConfiguration('mesh_suffix', default='stl')
+    kinematics_suffix = LaunchConfiguration('kinematics_suffix', default='')
+    add_gripper = LaunchConfiguration('add_gripper', default=True)
+    add_vacuum_gripper = LaunchConfiguration('add_vacuum_gripper', default=False)
+    add_bio_gripper = LaunchConfiguration('add_bio_gripper', default=False)
+    add_realsense_d435i = LaunchConfiguration('add_realsense_d435i', default=False)
+    add_d435i_links = LaunchConfiguration('add_d435i_links', default=True)
+    add_other_geometry = LaunchConfiguration('add_other_geometry', default=False)
+    geometry_type = LaunchConfiguration('geometry_type', default='box')
+    geometry_mass = LaunchConfiguration('geometry_mass', default=0.1)
+    geometry_height = LaunchConfiguration('geometry_height', default=0.1)
+    geometry_radius = LaunchConfiguration('geometry_radius', default=0.1)
+    geometry_length = LaunchConfiguration('geometry_length', default=0.1)
+    geometry_width = LaunchConfiguration('geometry_width', default=0.1)
+    geometry_mesh_filename = LaunchConfiguration('geometry_mesh_filename', default='')
+    geometry_mesh_origin_xyz = LaunchConfiguration('geometry_mesh_origin_xyz', default='"0 0 0"')
+    geometry_mesh_origin_rpy = LaunchConfiguration('geometry_mesh_origin_rpy', default='"0 0 0"')
+    geometry_mesh_tcp_xyz = LaunchConfiguration('geometry_mesh_tcp_xyz', default='"0 0 0"')
+    geometry_mesh_tcp_rpy = LaunchConfiguration('geometry_mesh_tcp_rpy', default='"0 0 0"')
+
+    # Build MoveIt configuration
+    moveit_config_dump = LaunchConfiguration('moveit_config_dump', default='')
+    moveit_config_dump = moveit_config_dump.perform(context)
+    moveit_config_dict = yaml.load(moveit_config_dump, Loader=yaml.FullLoader) if moveit_config_dump else {}
+
+    if not moveit_config_dict:
+        moveit_config = MoveItConfigsBuilder(
+            context=context,
+            dof=dof,
+            robot_type=robot_type,
+            prefix=prefix,
+            hw_ns=hw_ns,
+            limited=limited,
+            effort_control=effort_control,
+            velocity_control=velocity_control,
+            model1300=model1300,
+            robot_sn=robot_sn,
+            attach_to=attach_to,
+            attach_xyz=attach_xyz,
+            attach_rpy=attach_rpy,
+            mesh_suffix=mesh_suffix,
+            kinematics_suffix=kinematics_suffix,
+            add_gripper=add_gripper,
+            add_vacuum_gripper=add_vacuum_gripper,
+            add_bio_gripper=add_bio_gripper,
+            add_realsense_d435i=add_realsense_d435i,
+            add_d435i_links=add_d435i_links,
+            add_other_geometry=add_other_geometry,
+            geometry_type=geometry_type,
+            geometry_mass=geometry_mass,
+            geometry_height=geometry_height,
+            geometry_radius=geometry_radius,
+            geometry_length=geometry_length,
+            geometry_width=geometry_width,
+            geometry_mesh_filename=geometry_mesh_filename,
+            geometry_mesh_origin_xyz=geometry_mesh_origin_xyz,
+            geometry_mesh_origin_rpy=geometry_mesh_origin_rpy,
+            geometry_mesh_tcp_xyz=geometry_mesh_tcp_xyz,
+            geometry_mesh_tcp_rpy=geometry_mesh_tcp_rpy,
+        ).to_moveit_configs()
+        moveit_config_dict = moveit_config.to_dict()
+    
+    # Prepare MoveGroup interface parameters
+    move_group_params = {
+        'robot_description': moveit_config_dict['robot_description'],
+        'robot_description_semantic': moveit_config_dict['robot_description_semantic'],
+        'robot_description_kinematics': moveit_config_dict['robot_description_kinematics'],
+        'group_name': LaunchConfiguration('group_name', default='xarm7'),
+        'robot_type': robot_type,
+        'dof': dof,
+        'prefix': prefix
+    }
+
+    
+    # Create a multithreaded component container
+    container = ComposableNodeContainer(
+        name='state_machine_container',
+        namespace='',
+        package='rclcpp_components',
+        executable='component_container_mt',  # Multi-threaded container
+        parameters=[
+            {'use_multi_threaded_executor': True}
+        ],
+        composable_node_descriptions=[
+            ComposableNode(
+                package='xarm7_hybrid_state_machine',
+                plugin='simple_state_machine::PickAndPlaceStateMachine',
+                name='pick_and_place_sm_node',
+                parameters=[move_group_params],
+                extra_arguments=[{'use_intra_process_comms': True}]
+            ),
+        ],
+        output='screen',
+    )
+
+    return [container]
+
+def generate_launch_description():
+    """Generate launch description with multithreaded component container."""
+    return LaunchDescription([
+        OpaqueFunction(function=launch_setup)
+    ])

1d2c5ac4edae772f532ec819defd55b5f0c3448a
newtonjeri <newtonkaris45@gmail.com>
2025-09-10 15:02:14 +0300

    feat: implement Pick and Place service node with necessary callbacks and state management

---
+++ xarm7_hybrid_state_machine/include/xarm7_hybrid_state_machine/pick_and_place_service.hpp
@@ -0,0 +1,84 @@
+/* Copyright 2025 Virtual Mechatronics Labs DeKUT All Rights Reserved.
+ *
+ * Software License Agreement (BSD License)
+ *
+ * Author: Newton Kariuki <newtonkaris45@gmail.com>
+ ============================================================================*/
+
+#ifndef XARM7_HYBRID_STATE_MACHINE__PICK_AND_PLACE_SERVICE_HPP_
+#define XARM7_HYBRID_STATE_MACHINE__PICK_AND_PLACE_SERVICE_HPP_
+
+#include <memory>
+#include <string>
+#include <map>
+#include <chrono>
+
+#include "rclcpp/rclcpp.hpp"
+#include "xarm_msgs/srv/pick_and_place_service.hpp"
+#include "xarm_msgs/msg/robot_state_and_target_pose.hpp"
+#include "std_msgs/msg/u_int8.hpp"
+#include "geometry_msgs/msg/pose.hpp"
+#include <moveit/planning_scene_interface/planning_scene_interface.hpp>
+
+using namespace std::chrono_literals;
+
+namespace pick_and_place_service
+{
+    enum PART_TYPES
+    {
+        SPINDLE_2 = 1,
+        PINION_GEAR = 2,
+        IDLER_GEAR = 3,
+        COVER_PLATE = 4
+    };
+
+    class PickAndPlaceServiceNode : public rclcpp::Node
+    {
+    public:
+        PickAndPlaceServiceNode(const rclcpp::NodeOptions &options = rclcpp::NodeOptions());
+        ~PickAndPlaceServiceNode();
+
+    private:
+        // Service callback
+        void pickAndPlaceService(
+            const std::shared_ptr<xarm_msgs::srv::PickAndPlaceService::Request> request,
+            std::shared_ptr<xarm_msgs::srv::PickAndPlaceService::Response> response);
+
+        // State machine monitoring callback
+        void stateMachineStateCallback(const std_msgs::msg::UInt8::SharedPtr msg);
+
+        // Helper functions
+        bool isValidPartName(const std::string& part_name);
+        int getPartId(const std::string& part_name);
+        std::map<std::string, geometry_msgs::msg::Pose> getPartPoses(const std::string& part_name);
+        void sendPickAndPlaceCommand(int part_id, const std::map<std::string, geometry_msgs::msg::Pose>& poses);
+        bool waitForCompletion(const std::chrono::seconds& timeout);
+
+        // Publishers and subscribers
+        rclcpp::Service<xarm_msgs::srv::PickAndPlaceService>::SharedPtr pick_place_service_;
+        rclcpp::Publisher<xarm_msgs::msg::RobotStateAndTargetPose>::SharedPtr state_topic_publisher_;
+        rclcpp::Subscription<std_msgs::msg::UInt8>::SharedPtr state_machine_subscriber_;
+
+        // State monitoring
+        uint8_t current_state_machine_state_;
+        bool operation_in_progress_;
+        bool operation_completed_;
+        bool operation_successful_;
+        
+        // Planning scene interface for collision objects
+        moveit::planning_interface::PlanningSceneInterface psi_;
+
+        // Mutex for thread safety
+        std::mutex state_mutex_;
+
+        // Timer for operation timeout
+        rclcpp::TimerBase::SharedPtr timeout_timer_;
+
+        // Predefined poses from experiment_002.cpp
+        void initializePoses();
+        std::map<std::string, std::map<std::string, geometry_msgs::msg::Pose>> part_poses_;
+    };
+
+} // namespace pick_and_place_service
+
+#endif // XARM7_HYBRID_STATE_MACHINE__PICK_AND_PLACE_SERVICE_HPP_

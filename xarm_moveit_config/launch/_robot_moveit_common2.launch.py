b48411f6100be654911eafcd1c421b3b4ad3aea6
newtonjeri <newtonkaris45@gmail.com>
2025-09-19 17:51:48 +0300

    feat: add trajectory execution configuration and move group capabilities to launch setup

--- xarm_moveit_config/launch/_robot_moveit_common2.launch.py
+++ xarm_moveit_config/launch/_robot_moveit_common2.launch.py
@@ -37,6 +37,20 @@ def launch_setup(context, *args, **kwargs):
     moveit_config_dict = yaml.load(moveit_config_dump, Loader=yaml.FullLoader)
     moveit_config_package_name = 'xarm_moveit_config'
 
+    
+
+        # Trajectory Execution Configuration
+    trajectory_execution = {
+        'moveit_manage_controllers': True,
+        'trajectory_execution.allowed_execution_duration_scaling': 1.2,
+        'trajectory_execution.allowed_goal_duration_margin': 0.5,
+        'trajectory_execution.allowed_start_tolerance': 0.01,
+        'trajectory_execution.execution_duration_monitoring': False
+    }
+
+
+    move_group_capabilities =  {"capabilities": "move_group/ExecuteTaskSolutionCapability"}
+
     # Start the actual move_group node/action server
     move_group_node = Node(
         package='moveit_ros_move_group',
@@ -45,6 +59,8 @@ def launch_setup(context, *args, **kwargs):
         parameters=[
             moveit_config_dict,
             {'use_sim_time': use_sim_time},
+            trajectory_execution,
+            move_group_capabilities,
         ],
     )
 
